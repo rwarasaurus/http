0c6d7823715e86356cb9491a222337fe34fe610e
Kieron <info@kieronwilson.co.uk>
2016-07-06 16:17:48 +0100

    updates

---
+++ src/Http/Emitter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Http;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+class Emitter {
+
+	public function emit(ResponseInterface $response) {
+		if(true === headers_sent()) {
+			throw new \RuntimeException('headers already sent.');
+		}
+
+		header(sprintf('%s %s %s',
+			$response->getProtocolVersion(),
+			$response->getStatusCode(),
+			$response->getReasonPhrase()
+		));
+
+		foreach($response->getHeaders() as $name => $values) {
+			foreach($values as $value) {
+				header(sprintf('%s: %s', $name, $value));
+			}
+		}
+
+		echo $response->getBody();
+	}
+
+}
